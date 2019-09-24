0af83ae13179125039aba77dc7b1e7816a3c9ea9
merjench <merjen.chary@gmail.com>
2019-09-23 23:45:21 -0400

    intersection of two different arrays ES6 magic solution

--- Array/intersection_of_two_arrays.js
+++ Array/intersection_of_two_arrays.js
@@ -1,6 +1,28 @@
 // We have two arrays:
 
-// The intersection needs to return the elements that both arrays share in common, in the case below it [1,5]
 
  arrA=[1,3,4,5]
  arrB=[1,2,5,6,7]
+
+ // The intersection needs to return the elements that both arrays share in common, in the case below above [1,5]
+
+ let intersection = arrA.filter(x => arrB.includes(x));
+
+// The difference will be output the elemts from Array A that are not in the array B. The results will be [3,4]
+
+let difference = arrA.filter(x => !arrB.includes(x));
+
+// Symmetrical Difference: in the case you will get an array containing all the elements of both arays that are not in the other array
+// so results: [2,3,5,6,7]
+
+let difference = arrA
+                 .filter(x => !arrB.includes(x))
+                 .concat(arrB.filter(x => !arrA.includes(x)));
+
+// Union: compabingin all the elements from first array into second array
+
+let union = [...arrA, ...arrB]
+   //or
+let union = [...new Set ([...arrA, ...arrB])];
+
+////---------------------------- Plain Vanilla ES6 Solution-------------///////////////////
