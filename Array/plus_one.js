08f4228e9179bbacd1d8458ba73bd0b677094ec7
merjench <merjen.chary@gmail.com>
2019-09-24 00:31:35 -0400

    Sudoku array

--- Array/plus_one.js
+++ Array/plus_one.js
@@ -12,4 +12,28 @@ Input: [4,3,2,1]
 Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
 
-// ---------Solution 1 ------------------// 
+// ---------Solution 1 ------------------//
+
+
+var plusOne = function(digits) {
+
+for(var i = digits.length - 1; i >= 0; i--) {
+    if(++digits[i] > 9) digits[i] = 0;
+    else return digits;
+  }
+    digits.unshift(1);
+    return digits;
+};
+
+//------------Solution 2  es6 ---------///
+
+const plusOne = digits => {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++
+            return digits
+        }
+        digits[i] = 0
+    }
+    return [1, ...digits]
+};
