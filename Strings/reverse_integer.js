a65138a38d74deb7f2acdc8930ed30458e5b635c
merjench <merjen.chary@gmail.com>
2019-09-24 01:11:49 -0400

    reverse int solution

--- Strings/reverse_integer.js
+++ Strings/reverse_integer.js
@@ -16,3 +16,31 @@ Input: 120
 Output: 21
 
 //-------------------Solution 1 ----------------------//
+
+// var reverse = function(x) {
+//   const reversed = x
+//   .toString()
+//   .split('')
+//   .reverse()
+//   .join('');
+
+//   return parseInt(reversed) * Math.sign(x);
+// }
+
+//---------Solution 2------------------------------//
+var reverse = function(x) {
+     // check if negative
+  const negative = x < 0 && "-";
+
+  // create array from number
+  const array = Array.from(x.toString()).map(Number);
+
+  // reverse array and parse into an integer
+  const num = parseInt(array.reverse().join(""));
+
+  // if greater than the max 32bit integer return 0
+  if (num > 2147483647) return 0;
+
+  return parseInt(negative + num);
+
+};
