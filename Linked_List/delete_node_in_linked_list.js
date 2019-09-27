c6daf8f044c6d183580a3e519cf3801e02f11f85
merjench <merjen.chary@gmail.com>
2019-09-27 15:20:42 -0400

    delete node solution

--- Linked_List/delete_node_in_linked_list.js
+++ Linked_List/delete_node_in_linked_list.js
@@ -14,3 +14,19 @@ Note:
 •	All of the nodes' values will be unique.
 •	The given node will not be the tail and it will always be a valid node of the linked list.
 •	Do not return anything from your function.
+
+
+//-------------------Solution 1----------------------//
+
+var deleteNode = function(node) {
+    if (!node.next) return;
+    // If we reach here, we have at least 2 nodes
+    // We stop on the 2nd to last node
+    while (node.next.next) {
+        node.val = node.next.val;
+        node = node.next;
+    }
+    // Get value of last node and delete last node
+    node.val = node.next.val
+    node.next = null;
+};
